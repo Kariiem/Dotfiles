859eeddd2345534ddc172e6e4b40960fe37b9d1b
Kariiem <krm.taha@outlook.com>
2023-02-18 20:38:30 +0200

    Just use Doom

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -1,13 +1,9 @@
-(org-babel-load-file
-  (expand-file-name "config.org" user-emacs-directory) t)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(evil-org zig-mode yasnippet-snippets vertico use-package toc-org sml-mode setup org-superstar marginalia lispy haskell-mode gruber-darker-theme fsharp-mode evil esup doom-themes company))
- '(zig-format-on-save nil))
+ '(org-agenda-files nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
