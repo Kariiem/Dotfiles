37ebedbc102c7d7c57bb33bb34fde3af228520a1
Karim Taha <kariem.taha2.7@gmail.com>
2023-08-13 05:27:34 +0200

    on FreeBSD I use emacs 28, which misses `defvar-keymap` macro

--- vanilla-emacs/lisp/init-window.el
+++ vanilla-emacs/lisp/init-window.el
@@ -1,22 +1,25 @@
 (defalias 'shrink-window-vertically 'shrink-window)
 (defalias 'enlarge-window-vertically 'enlarge-window)
-(define-prefix-command 'window-ctrl)
 
-(defvar-keymap window-ctrl-map
-  :prefix 'window-ctrl
-  "c"       'delete-window
-  "v"       'split-window-right
-  "s"       'split-window-below
-  "<up>"    'enlarge-window-vertically
-  "<down>"  'shrink-window-vertically
-  "<right>" 'enlarge-window-horizontally
-  "<left>"  'shrink-window-horizontally)
-
-(put 'enlarge-window-vertically  'repeat-map 'window-ctrl-map)
+(define-minor-mode window-ctrl "" :global t)
+(defvar window-ctrl-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "c") (repeat-command! delete-window))
+    (define-key map (kbd "v") 'split-window-right)
+    (define-key map (kbd "s") 'split-window-below)
+    (define-key map (kbd "<up>") (repeat-command! enlarge-window-vertically))
+    (define-key map (kbd "<down>") (repeat-command! shrink-window-vertically))
+    (define-key map (kbd "<right>") (repeat-command! enlarge-window-horizontally))
+    (define-key map (kbd "<left>") (repeat-command! shrink-window-horizontally))
+    map)
+  "")
+(put 'enlarge-window-vertically 'repeat-map 'window-ctrl-map)
 (put 'shrink-window-vertically 'repeat-map 'window-ctrl-map)
 (put 'enlarge-window-horizontally 'repeat-map 'window-ctrl-map)
 (put 'shrink-window-horizontally 'repeat-map 'window-ctrl-map)
 (put 'delete-window 'repeat-map 'window-ctrl-map)
 
-(global-set-key (kbd "C-c w") window-ctrl)
+(global-set-key (kbd "C-c w") window-ctrl-map)
+(window-ctrl 1)
+
 (provide 'init-window)
