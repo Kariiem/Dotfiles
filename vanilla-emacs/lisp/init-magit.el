0173a0d8d127ed310c685d9f6c407c29b023adce
Kariiem <krm.taha@outlook.com>
2023-09-01 14:07:52 +0300

    change magit log-margin format

--- vanilla-emacs/lisp/init-magit.el
+++ vanilla-emacs/lisp/init-magit.el
@@ -1,13 +1,14 @@
 (require-package 'magit)
 
 (with-eval-after-load 'magit
-    (transient-define-argument magit-commit:--reedit-message ()
-      :description "Reedit commit message"
-      :class 'transient-option
-      :shortarg "-c"
-      :argument "--reedit-message="
-      :reader #'magit-read-reedit-message
-      :history-key 'magit-revision-history)
+  (transient-define-argument magit-commit:--reedit-message ()
+    :description "Reedit commit message"
+    :class 'transient-option
+    :shortarg "-c"
+    :argument "--reedit-message="
+    :reader #'magit-read-reedit-message
+    :history-key 'magit-revision-history)
+  (setq magit-log-margin '(t "%Y-%m-%d %R" magit-log-margin-width t 18))
 
   (defun magit-read-reedit-message (prompt &optional default history)
     (magit-completing-read prompt (magit-list-refnames)
@@ -15,6 +16,6 @@
                            (or default
                                (and (magit-rev-verify "ORIG_HEAD")
                                     "ORIG_HEAD"))))
-  (transient-append-suffix 'magit-commit "-C"
-    '(magit-commit:--reedit-message)))
+
+  (transient-append-suffix 'magit-commit "-C" '(magit-commit:--reedit-message)))
 (provide 'init-magit)
