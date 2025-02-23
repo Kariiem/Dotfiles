bc8b4ebfd7b2c9f865b290716c98f8177689b49d
Karim Taha <kariem.taha2.7@gmail.com>
2025-02-23 08:08:32 +0200

    org latex conf

--- .emacs.d/lisp/init-org.el
+++ .emacs.d/lisp/init-org.el
@@ -1,6 +1,7 @@
 ;; -*- lexical-binding: t -*-
 
 (install-pkgs org-tidy)
+
 (defun position-link ()
   (interactive)
   (kill-new
@@ -15,25 +16,28 @@
       org-src-tab-acts-natively t
       org-confirm-babel-evaluate nil
       org-id-locations-file-relative t
+      org-ellipsis "\n➤"
       org-edit-src-content-indentation 0)
 
 (defun tangle-in-org (&optional arg target-file lang-re)
   (when (string-equal major-mode 'org-mode)
     (org-babel-tangle arg target-file lang-re)))
 
+
+(add-hook 'org-mode-hook #'org-tidy-mode)
+;;(add-hook 'prog-mode-hook (local-set-key (kbd "C-c c") 'position-link))
+
 (with-eval-after-load 'org
+  (require 'ox-extra)
+  (require 'org-tempo)
+  (ox-extras-activate '(latex-header-blocks ignore-headlines))
   (add-to-list 'org-src-lang-modes '("haskell" . haskell))
 
-  (add-hook 'org-mode-hook (lambda ()
-                             (add-hook 'after-save-hook 'tangle-in-org)))
-  (add-hook 'org-mode-hook #'org-tidy-mode)
-  (require 'org-tempo)
+  (add-hook 'after-save-hook 'tangle-in-org)
+
   (global-set-key (kbd "C-c s") #'org-store-link)
   (global-set-key (kbd "C-c l") #'org-insert-last-stored-link)
   (global-set-key (kbd "C-c a") #'org-agenda)
   (global-set-key (kbd "C-c c") #'org-capture))
-
-(add-hook 'prog-mode-hook (lambda ()
-                            (local-set-key (kbd "C-c c") 'position-link)))
 
 (provide 'init-org)
