ac2ce11720118466163adab8b1afa6f8dd1b108c
Karim Taha <kariem.taha2.7@gmail.com>
2026-06-20 15:41:03 +0300

    chore: add ruby related files to auto-mode-alist with ruby mode.

--- .emacs.d/lisp/init-ruby.el
+++ .emacs.d/lisp/init-ruby.el
@@ -1,10 +1,15 @@
 ;; -*- lexical-binding: t; -*-
 
-(install-pkgs ruby-end
-              inf-ruby
-              robe)
+(install-pkgs inf-ruby
+              robe
+              ruby-electric)
 
 (add-hook 'ruby-mode-hook 'robe-mode)
-(add-hook 'ruby-ts-mode-hook 'robe-mode)
+(add-hook 'ruby-mode-hook 'ruby-electric-mode)
+
+(add-to-list 'auto-mode-alist
+             '("\\.\\(?:cap\\|gemspec\\|irbrc\\|gemrc\\|rake\\|rb\\|ru\\|thor\\)\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist
+             '("\\(?:Brewfile\\|Capfile\\|Gemfile\\(?:\\.[a-zA-Z0-9._-]+\\)?\\|[rR]akefile\\)\\'" . ruby-mode))
 
 (provide 'init-ruby)
